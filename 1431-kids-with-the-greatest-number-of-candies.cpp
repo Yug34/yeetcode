862ee5efc8debe354ff956effebeb51ecd701fdd
Yug34 <vertilax1337@gmail.com>
2021-01-01 20:56:32 +0530

    1431-kids-with-the-greatest-number-of-candies

---
+++ 1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int len = candies.size();
+        int max = candies[0];
+
+        for (int val : candies) {
+            if (val > max) {
+                max = val;
+            }
+        }
+
+        vector<bool> ans;
+        for (int val : candies) {
+            if((val + extraCandies) >= max) {
+                ans.push_back(true);
+                continue;
+            }
+            ans.push_back(false);
+        }
+
+        return ans;
+    }
+};
